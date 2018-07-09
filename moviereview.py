61b9c593aaa7a821598ce3de37cf528c94fc39b4
Richa Singh <rockingrichie1994@gmail.com>
2018-07-08 19:52:21 -0500

    Update moviereview.py

--- moviereview.py
+++ moviereview.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import torch
 import torch.nn as nn 
-import torch.nn.paralle
+import torch.nn.parallel
 import torch.optim as optim
 import torch.utils.data
 from torch.autograd import Variable
